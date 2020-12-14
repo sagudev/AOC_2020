918c9b22e052e6c015b18a1e5b6fee083f24a216
sagudev <samo.golez@outlook.com>
2020-12-14 11:54:06 +0100

    day14 p2

--- src/bin/day14.rs
+++ src/bin/day14.rs
@@ -1,11 +1,10 @@
-use std::collections::HashMap;
 use aoc::read_data;
-use std::borrow::Borrow;
+use std::collections::HashMap;
 use std::error::Error;
 
 #[derive(Debug)]
 // loc, binary
-struct Mem(usize, String);
+struct Mem(String, String);
 
 #[derive(Debug)]
 struct Section {
@@ -21,8 +20,16 @@ impl Section {
             let end_mem = d.find(']').unwrap();
             let e = d.find('=').unwrap();
             mem.push(Mem(
-                d[start_mem+1..end_mem].parse().unwrap(),
-                format!("{:0width$b}", d[e + 2..].parse::<usize>().unwrap(), width = 36),
+                format!(
+                    "{:0width$b}",
+                    d[start_mem + 1..end_mem].parse::<usize>().unwrap(),
+                    width = 36
+                ),
+                format!(
+                    "{:0width$b}",
+                    d[e + 2..].parse::<usize>().unwrap(),
+                    width = 36
+                ),
             ));
         }
         Self {
@@ -53,7 +60,7 @@ struct Machine {
 impl Machine {
     fn new() -> Self {
         Self {
-            mem: HashMap::new()
+            mem: HashMap::new(),
         }
     }
     fn aps(&mut self, s: &Section) {
@@ -69,7 +76,47 @@ impl Machine {
                 }
             }
             //println!("{}: {}", m.0, usize::from_str_radix(&masked, 2).unwrap());
-            self.mem.insert(m.0, usize::from_str_radix(&masked, 2).unwrap());
+            self.mem.insert(
+                usize::from_str_radix(&m.0, 2).unwrap(),
+                usize::from_str_radix(&masked, 2).unwrap(),
+            );
+        }
+    }
+    fn aps2(&mut self, s: &Section) {
+        let mask: Vec<char> = s.mask.chars().collect();
+        for m in &s.mem {
+            let mut masked = String::new();
+            let mut num: usize = 0;
+            for (i, c) in m.0.chars().enumerate() {
+                match mask[i] {
+                    'X' => {
+                        masked += "X";
+                        num += 1
+                    }
+                    '1' => masked += "1",
+                    '0' => masked += &c.to_string(),
+                    _ => panic!("err"),
+                }
+            }
+            for d in 0..2_i32.pow(num as u32) {
+                let s = format!("{:0width$b}", d, width = num);
+                let mut s = s.chars();
+                self.mem.insert(
+                    usize::from_str_radix(
+                        &masked.chars().map(|x| {
+                            if x == 'X' {
+                                s.next().unwrap()
+                            } else {
+                                x
+                            }
+                        }).collect::<String>(),
+                        2,
+                    )
+                    .unwrap(),
+                    usize::from_str_radix(&m.1, 2).unwrap(),
+                );
+            }
+            //println!("{}: {}", m.0, usize::from_str_radix(&masked, 2).unwrap());
         }
     }
 }
@@ -88,8 +135,16 @@ fn p1(data: &[Section]) -> usize {
     res
 }
 
-fn p2(data: &[Section]) -> i32 {
-    5
+fn p2(data: &[Section]) -> usize {
+    let mut m = Machine::new();
+    for d in data {
+        m.aps2(d);
+    }
+    let mut res = 0;
+    for t in m.mem.values() {
+        res += t
+    }
+    res
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -102,7 +157,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         p1(&data)
     );
     // part 2
-    println!("Fuel needed (part 2): {}", p2(&data));
+    println!("Execute the initialization program using an emulator for a version 2 decoder chip. What is the sum of all values left in memory after it completes? {}", p2(&data));
     Ok(())
 }
 
@@ -120,7 +175,6 @@ fn calc() {
         "mem[8] = 0".to_string(),
     ];
     let data: Vec<Section> = trans(&data);
-    println!("{:?}", data);
     // part 1
     let mut m = Machine::new();
     m.aps(&data[0]);
@@ -128,4 +182,27 @@ fn calc() {
     assert_eq!(m.mem[&7], 101);
     assert_eq!(p1(&data), 165);
     // part 2
+    let data: Vec<String> = vec![
+        "mask = 000000000000000000000000000000X1001X".to_string(),
+        "mem[42] = 100".to_string(),
+        "mask = 00000000000000000000000000000000X0XX".to_string(),
+        "mem[26] = 1".to_string(),
+    ];
+    let data: Vec<Section> = trans(&data);
+    let mut m = Machine::new();
+    m.aps2(&data[0]);
+    assert_eq!(m.mem[&26], 100);
+    assert_eq!(m.mem[&27], 100);
+    assert_eq!(m.mem[&58], 100);
+    assert_eq!(m.mem[&59], 100);
+    m.aps2(&data[1]);
+    assert_eq!(m.mem[&16], 1);
+    assert_eq!(m.mem[&17], 1);
+    assert_eq!(m.mem[&18], 1);
+    assert_eq!(m.mem[&19], 1);
+    assert_eq!(m.mem[&24], 1);
+    assert_eq!(m.mem[&25], 1);
+    assert_eq!(m.mem[&26], 1);
+    assert_eq!(m.mem[&27], 1);
+    assert_eq!(p2(&data), 208);
 }
