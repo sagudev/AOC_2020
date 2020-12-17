6af6d24395c97564ca08a65d60bae99a340f2d81
sagudev <samo.golez@outlook.com>
2020-12-17 20:04:41 +0100

    day17 p2

--- src/bin/day17.rs
+++ src/bin/day17.rs
@@ -148,14 +148,14 @@ impl D3 {
                 c.row.push_front(Cell::Inactive);
                 c.row.push_back(Cell::Inactive);
             }
-            self.rows += 2;
             d2.yx.push_front(Row {
-                row: VecDeque::from_iter(vec![Cell::Inactive; self.rows]),
+                row: VecDeque::from_iter(vec![Cell::Inactive; self.rows + 2]),
             });
             d2.yx.push_back(Row {
-                row: VecDeque::from_iter(vec![Cell::Inactive; self.rows]),
+                row: VecDeque::from_iter(vec![Cell::Inactive; self.rows + 2]),
             });
         }
+        self.rows += 2;
         self.cols += 2;
         // extend z
         self.insert0(min - 1);
@@ -192,8 +192,8 @@ fn cikel(data: &mut D3) {
     data.extend(min, max);
     let mut res = data.clone();
     for (z, d2) in &data.z {
-        println!("||");
-        println!("{}", d2);
+        //println!("||");
+        //println!("{}", d2);
         for i in 0..d2.yx.len() {
             let d = &d2.yx[i];
             for j in 0..d.row.len() {
@@ -227,7 +227,7 @@ fn cikel(data: &mut D3) {
                     }
                 }
             }
-            println!();
+            //println!();
         }
     }
     *data = res;
@@ -252,8 +252,158 @@ fn p1(data: &D3) -> usize {
     count
 }
 
-fn p2(data: &D3) -> usize {
-    5
+#[derive(Debug, Clone)]
+struct D4 {
+    w: HashMap<i32, D3>,
+}
+
+impl D4 {
+    fn insert0(&mut self, w: i32) {
+        let z0 = self.w.get(&0).unwrap();
+        let (min, max) = (*z0.z.keys().min().unwrap(), *z0.z.keys().max().unwrap());
+        let r0 = VecDeque::from_iter(vec![Cell::Inactive; z0.rows]);
+        let mut z = HashMap::new();
+        for i in min..=max {
+            z.insert(
+                i,
+                D2 {
+                    yx: VecDeque::from_iter(vec![Row { row: r0.clone() }; z0.cols]),
+                },
+            );
+        }
+        self.w.insert(
+            w,
+            D3 {
+                rows: z0.rows,
+                cols: z0.rows,
+                z,
+            },
+        );
+    }
+    fn extend(&mut self, min: i32, max: i32) {
+        let z0 = self.w.get(&0).unwrap();
+        let (min, max) = (*z0.z.keys().min().unwrap(), *z0.z.keys().max().unwrap());
+        // extend every
+        for d3 in self.w.values_mut() {
+            d3.extend(min, max);
+        }
+        // extend z
+        self.insert0(min - 1);
+        self.insert0(max + 1);
+    }
+}
+
+fn trans4(d3: D3) -> D4 {
+    let mut w = HashMap::new();
+    w.insert(0, d3);
+    D4 { w }
+}
+
+impl std::fmt::Display for D4 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut s = String::new();
+        for (k, v) in &self.w {
+            s = format!("{}|||| {} ||||\n{}\n", s, k, v);
+        }
+        write!(f, "{}", s)
+    }
+}
+
+fn cikel4(data: &mut D4) {
+    let (min, max) = (*data.w.keys().min().unwrap(), *data.w.keys().max().unwrap());
+    // extend place
+    data.extend(min, max);
+    let mut res = data.clone();
+    for (w, d3) in &data.w {
+        for (z, d2) in &d3.z {
+            //println!("||");
+            //println!("{}", d2);
+            for i in 0..d2.yx.len() {
+                let d = &d2.yx[i];
+                for j in 0..d.row.len() {
+                    let cell = &d.row[j];
+                    // check in this w
+                    //  check in this z
+                    let mut a = d2.check(i, j);
+
+                    //  check in others z
+                    if let Some(x) = d3.z.get(&(z - 1)) {
+                        a += x.checkz(i, j);
+                    }
+                    if let Some(x) = d3.z.get(&(z + 1)) {
+                        a += x.checkz(i, j);
+                    }
+
+                    if let Some(xw) = data.w.get(&(w + 1)) {
+                        //  check in this z
+                        a += xw.z[z].checkz(i, j);
+
+                        //  check in others z
+                        if let Some(x) = xw.z.get(&(z - 1)) {
+                            a += x.checkz(i, j);
+                        }
+                        if let Some(x) = xw.z.get(&(z + 1)) {
+                            a += x.checkz(i, j);
+                        }
+                    }
+
+                    if let Some(xw) = data.w.get(&(w - 1)) {
+                        //  check in this z
+                        a += xw.z[z].checkz(i, j);
+
+                        //  check in others z
+                        if let Some(x) = xw.z.get(&(z - 1)) {
+                            a += x.checkz(i, j);
+                        }
+                        if let Some(x) = xw.z.get(&(z + 1)) {
+                            a += x.checkz(i, j);
+                        }
+                    }
+
+                    /*
+                    If a cube is active and exactly 2 or 3 of its neighbors are also active, the cube remains active. Otherwise, the cube becomes inactive.
+                    If a cube is inactive but exactly 3 of its neighbors are active, the cube becomes active. Otherwise, the cube remains inactive.
+                    */
+                    let c = &mut res.w.get_mut(w).unwrap().z.get_mut(z).unwrap().yx[i].row[j];
+                    match cell {
+                        Cell::Active => {
+                            if !(a == 3 || a == 2) {
+                                *c = Cell::Inactive;
+                            }
+                        }
+                        Cell::Inactive => {
+                            if a == 3 {
+                                *c = Cell::Active;
+                            }
+                        }
+                    }
+                }
+                //println!();
+            }
+        }
+    }
+    *data = res;
+}
+
+fn p2(data: &D4) -> usize {
+    let mut data = data.clone();
+    let mut count = 0;
+    // 6 ciklov
+    for _ in 1..=6 {
+        cikel4(&mut data)
+    }
+    for w in data.w.values() {
+        for v in w.z.values() {
+            for d in &v.yx {
+                for r in &d.row {
+                    if *r == Cell::Active {
+                        count += 1;
+                    }
+                }
+            }
+        }
+    }
+    count
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -265,8 +415,12 @@ fn main() -> Result<(), Box<dyn Error>> {
         "How many cubes are left in the active state after the sixth cycle? {}",
         p1(&data)
     );
+    let data4 = trans4(data);
     // part 2
-    println!("Given the new visibility method and the rule change for occupied seats becoming empty, once equilibrium is reached, how many seats end up occupied? {}", p2(&data));
+    println!(
+        "How many cubes are left in the active state after the sixth cycle in 4-dimensional space? {}",
+        p2(&data4)
+    );
     Ok(())
 }
 
@@ -285,11 +439,20 @@ fn calc17() {
     );
     let mut mdata = data.clone();
     // part 1
-    println!("{}", mdata);
-    cikel(&mut mdata);
-    println!("{}", mdata);
-    cikel(&mut mdata);
-    println!("{}", mdata);
+    //println!("{}", mdata);
+    //cikel(&mut mdata);
+    //println!("{}", mdata);
+    //cikel(&mut mdata);
+    //println!("{}", mdata);
     assert_eq!(p1(&data), 112);
     // part 2
+    let data4 = trans4(data);
+    let mut mdata4 = data4.clone();
+    // part 1
+    println!("{}", mdata4);
+    cikel4(&mut mdata4);
+    println!("{}", mdata4);
+    //cikel(&mut mdata4);
+    //println!("{}", mdata);
+    assert_eq!(p2(&data4), 848);
 }
