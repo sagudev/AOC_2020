1e92d8961b5eba387541bc88e7262533e88759c4
sagudev <samo.golez@outlook.com>
2020-12-19 18:44:28 +0100

    bk

--- src/bin/day19.rs
+++ src/bin/day19.rs
@@ -6,7 +6,7 @@ use std::str::FromStr;
 
 #[derive(Debug, Clone)]
 enum Expr {
-    Str(String),
+    Str(char),
     Ref(usize),
     Or(Box<Expr>, Box<Expr>),
     And(Vec<Expr>),
@@ -43,10 +43,55 @@ impl Expr {
                 y.noref(h);
             }
             Expr::Ref(y) => {
+                //println!("{}", y);
                 *self = h.get(&y).unwrap().clone();
                 self.noref(h);
             }
             Expr::Str(_) => {}
+        }
+    }
+    fn len(&self) -> usize {
+        match self {
+            Expr::And(x) => {
+                let mut c = 0;
+                for d in x {
+                    c += d.len();
+                }
+                c
+            }
+            Expr::Or(x, _) => x.len(),
+            Expr::Ref(_) => {
+                panic!("ref in len")
+            }
+            Expr::Str(_) => 1,
+        }
+    }
+    fn check(&self, i: usize, c: char) -> bool {
+        //println!("{}| {}:{}", self, i, c);
+        let mut i = i;
+        match self {
+            Expr::And(x) => {
+                for d in x {
+                    let len = d.len();
+                    if i < len {
+                        if !d.check(i, c) {
+                            return false
+                        }
+                    } else {
+                        i -= len;
+                    }
+                }
+                true
+            }
+            Expr::Or(x, y) => x.check(i, c) || y.check(i, c),
+            Expr::Ref(_) => panic!("ref in merged"),
+            Expr::Str(x) => {
+                if i == 0 {
+                    *x == c
+                } else {
+                    panic!("ref in merged {}, {}", x, i)
+                }
+            }
         }
     }
 }
@@ -55,7 +100,7 @@ impl FromStr for Expr {
     type Err = ParseIntError;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         if s.contains('"') {
-            Ok(Expr::Str(s.split('"').nth(1).unwrap().to_string()))
+            Ok(Expr::Str(s.trim().chars().nth(1).unwrap()))
         } else if s.contains('|') {
             let mut n11 = 0;
             let mut n12 = 0;
@@ -85,7 +130,7 @@ impl FromStr for Expr {
                     Box::new(Expr::And(vec![Expr::Ref(n21), Expr::Ref(n22)])),
                 ))
             } else {
-                Ok(Expr::Or(Box::new(Expr::Ref(n11)), Box::new(Expr::Ref(n21))))
+                Ok(Expr::Or(Box::new(Expr::Ref(n11)), Box::new(Expr::Ref(n12))))
             }
         } else {
             let mut v = Vec::new();
@@ -112,7 +157,7 @@ fn rule(s: &str) -> Expr {
         let mut l = l.split(':');
         let i: usize = l.next().unwrap().parse().unwrap();
         let e: Expr = l.next().unwrap().parse().unwrap();
-        //println!("//{} || {:?}", i, e);
+        //println!("//{} || {}", i, e);
         hm.insert(i, e);
     }
     // now merge all in rule No. 0
@@ -122,21 +167,47 @@ fn rule(s: &str) -> Expr {
     o
 }
 
-fn p1(data: &[String]) -> i32 {
-    5
+fn check(s: &str, r: &Expr) -> bool {
+    for (i, c) in s.trim().chars().enumerate() {
+        if !r.check(i, c) {
+            return false;
+        }
+    }
+    true
 }
 
-fn p2(data: &[String]) -> i32 {
+fn p1(data: &[&str], r: &Expr) -> i32 {
+    let mut c = 0;
+    for d in data {
+        if check(d, r) {
+            c += 1;
+        }
+    }
+    c
+}
+
+fn p2(data: &[&str]) -> i32 {
     5
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     println!("Hello, Advent Of Code 2020!");
     let data: Vec<String> = read_data_space_saperator2("./data/data19", '$').unwrap();
+    //let datac = data.clone();
+    //let rule = std::thread::Builder::new()
+    //    .stack_size(10_000_000 as usize * 0xFF)
+    //    .spawn(move || rule(&datac[0].clone()))
+    //    .unwrap()
+    //    .join()
+    //    .unwrap();
     let rule = rule(&data[0]);
-    println!("{}", rule);
+    let data: Vec<&str> = data[1].split('$').filter(|x| *x != "").collect();
+    println!("{:?}", rule);
     // part 1
-    println!("How many messages completely match rule 0? {}", p1(&data));
+    println!(
+        "How many messages completely match rule 0? {}",
+        p1(&data, &rule)
+    );
     // part 2
     println!("How many messages completely match rule 0? {}", p2(&data));
     Ok(())
@@ -157,6 +228,14 @@ fn calc19() {
         "$ababbb$bababa$abbbab$aaabbb$aaaabbb".to_string(),
     ];
     let rule = rule(&data[0]);
+    let data: Vec<&str> = data[1].split('$').filter(|x| *x != "").collect();
     println!("{}", rule);
+    println!("{:?}", data);
     // part 1
+    assert_eq!(check(&data[0], &rule), true);
+    assert_eq!(check(&data[1], &rule), false);
+    assert_eq!(check(&data[2], &rule), true);
+    assert_eq!(check(&data[3], &rule), false);
+    assert_eq!(check(&data[4], &rule), false);
+    assert_eq!(p1(&data, &rule), 2);
 }
